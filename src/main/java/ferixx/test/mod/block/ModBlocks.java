91827b1a915a7972a00cea27adadd8bd10f1c5ec
felixtingyh <felixtingyh@gmail.com>
2022-03-18 21:05:11 +0800

    [Update] Separate block and item registry to different files

---
+++ src/main/java/ferixx/test/mod/block/ModBlocks.java
@@ -0,0 +1,48 @@
+package ferixx.test.mod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import ferixx.test.mod.TestMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+    public static final Block NETHER_STAR_BLOCK = registerBlock("nether_star_block",
+            new Block(FabricBlockSettings
+            .of(Material.METAL)
+            .strength(50.0F, 1200.0F)
+            .requiresTool()
+            .sounds(BlockSoundGroup.NETHERITE)
+            ), ItemGroup.MISC);
+
+    public static final Block NETHER_STAR_BRICKS = registerBlock("nether_star_bricks",
+            new Block(FabricBlockSettings
+            .of(Material.METAL)
+            .strength(50.0F, 1200.0F)
+            .requiresTool()
+            .sounds(BlockSoundGroup.NETHERITE)
+            ), ItemGroup.MISC);
+
+
+
+
+    private static Block registerBlock(String name, Block block, ItemGroup group) {
+        registerBlockItem(name, block, group);
+        return Registry.register(Registry.BLOCK, new Identifier(TestMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup group) {
+        return Registry.register(Registry.ITEM, new Identifier(TestMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)));
+    }
+
+    public static void registerModBlocks() {
+        TestMod.LOGGER.info("Registering ModBlocks for " + TestMod.MOD_ID);
+    }
+}
